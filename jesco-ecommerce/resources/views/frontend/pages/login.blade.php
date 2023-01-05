a82e09baa4c91dd0e352025afe4901c8807c6ea2
Jeshaiah Jesse <92147852+Jeshaiah04@users.noreply.github.com>
2023-01-06 01:15:34 +0700

    Final FIX
    External Collaboration in Share Session PHP Storm

---
+++ jesco-ecommerce/resources/views/frontend/pages/login.blade.php
@@ -0,0 +1,88 @@
+@extends('frontend.layouts.master')
+
+@section('title','JESCO|| Login Page')
+
+@section('main-content')
+    <!-- Breadcrumbs -->
+    <div class="breadcrumbs">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="bread-inner">
+                        <ul class="bread-list">
+                            <li><a href="{{route('home')}}">Home<i class="ti-arrow-right"></i></a></li>
+                            <li class="active"><a href="javascript:void(0);">Login</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Breadcrumbs -->
+
+    <!-- Shop Login -->
+    <section class="shop login section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 offset-lg-3 col-12">
+                    <div class="login-form">
+                        <h2>Login</h2>
+                        <p>Please Register To Order More Quickly!</p>
+                        <!-- Form -->
+                        <form class="form" method="post" action="{{route('login.submit')}}">
+                            @csrf
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <label>Email<span>*</span></label>
+                                        <input type="email" name="email" placeholder="" required="required" value="{{old('email')}}">
+                                        @error('email')
+                                            <span class="text-danger">{{$message}}</span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <label>Password<span>*</span></label>
+                                        <input type="password" name="password" placeholder="" required="required" value="{{old('password')}}">
+                                        @error('password')
+                                            <span class="text-danger">{{$message}}</span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group login-btn text-center">
+                                        <button class="btn" type="submit">Login</button>
+                                        <a href="{{route('register.form')}}" class="btn">Register</a>
+                                    </div>
+                                </div>
+
+                                <div class="col-12 text-center">
+                                        <div class="checkbox">
+                                            <label class="checkbox-inline" for="2"><input name="news" id="2" type="checkbox">Remember me</label>
+                                        </div>
+                                </div>
+                            </div>
+                        </form>
+                        <!--/ End Form -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--/ End Login -->
+@endsection
+@push('styles')
+<style>
+    .shop.login .form .btn{
+        background: #02254B;
+        margin-right:0;
+    }
+    .shop.login .form .btn:hover{
+        border-color: #02254B;
+        color: #02254B;
+        background: #fff;
+        margin-right:0;
+    }
+</style>
+@endpush
